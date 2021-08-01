1d585c8dc9ab08638afbb350157ce8ea13627e33
MrValanda <dota.doctor@mail.ru>
2021-08-01 17:00:54 +0300

    first commit

---
+++ Password.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace GenPassword
+{
+    public class Password
+    {
+        private string _setSymbols, _symbols;
+            private StringBuilder _password;
+            private int _size;
+
+            private bool _containsEngUpperLetter,
+                _containsNumber,
+                _containsSymbol,
+                _containsEngLowerLetter,
+                _containsRusUpperLetter,
+                _containsRusLowerLetter;
+
+            public Password(string symbols, int length, bool containsRusUpperLetter=false, bool containsRusLowerLetter=false, bool containsEngUpperLetter=false, bool containsNumber=false,
+                bool containsSymbol=false, bool containsEngLowerLetter=false)
+            {
+                _symbols = symbols;
+                _size = length;
+                _containsRusUpperLetter = containsRusUpperLetter;
+                _containsRusLowerLetter = containsRusLowerLetter;
+                _containsEngUpperLetter = containsEngUpperLetter;
+                _containsNumber = containsNumber;
+                _containsSymbol = containsSymbol;
+                _containsEngLowerLetter = containsEngLowerLetter;
+                for (int i = 0; i < length; i++)
+                {
+                    _setSymbols += (char) i;
+                }
+
+                _password = new StringBuilder(_setSymbols);
+            }
+
+            public void GenerateFinalPassword()
+            {
+                if (!_containsNumber && !_containsEngUpperLetter && !_containsSymbol && !_containsEngLowerLetter &&
+                    !_containsRusLowerLetter && !_containsRusUpperLetter)
+                    return;
+                int[] passwordIndex = new int[_size];
+                Random rand = new Random();
+                passwordIndex = passwordIndex.Select(x => -1).ToArray();
+                for (int i = 0; i < _size;)
+                {
+
+                    if (_containsEngUpperLetter && i < _size)
+                        CheckContainsAndAddChar(ref i, passwordIndex, rand, 'A', 'Z' + 1);
+                    if (_containsEngLowerLetter && i < _size)
+                        CheckContainsAndAddChar(ref i, passwordIndex, rand, 'a', 'z' + 1);
+
+                    if (_containsRusUpperLetter && i < _size)
+                        CheckContainsAndAddChar(ref i, passwordIndex, rand, 'А', 'Я' + 1);
+                    
+                    if (_containsRusLowerLetter && i < _size)
+                        CheckContainsAndAddChar(ref i, passwordIndex, rand, 'а', 'я' + 1);
+
+                    if (_containsSymbol && i < _size)
+                        CheckContainsAndAddChar(ref i, passwordIndex, rand);
+
+                    if (_containsNumber && i < _size)
+                        CheckContainsAndAddChar(ref i, passwordIndex, rand, '0', '9' + 1);
+                }
+            }
+
+            public StringBuilder GetPassword()
+            {
+                return _password;
+            }
+
+            private void CheckContainsAndAddChar(ref int i, int[] passwordIndex, Random rand, int left = 0,
+                int right = 0)
+            {
+                int tempIndex = rand.Next(0, _size);
+                while (passwordIndex.Contains(tempIndex)) tempIndex = rand.Next(0, _size);
+                
+                if (left != 0 && right != 0)
+                {
+                    char letter = (char) rand.Next(left, right);
+                    _password[tempIndex] = letter;
+                }
+                else
+                    _password[tempIndex] = _symbols[rand.Next(0, _symbols.Length)];
+
+                passwordIndex[i] = tempIndex;
+
+                i++;
+            }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using GenPassword;
+
+namespace ShifrXor
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write Text");
+            string text = Console.ReadLine();
+            Password genKey = new Password(@"#!@$%^&*_", text.Length, true, true,
+                true, true, true, true);
+            genKey.GenerateFinalPassword();
+            StringBuilder key = genKey.GetPassword();
+            
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Key");
+            Console.ResetColor();
+            
+            Console.WriteLine(key);
+            var textBinary = ConvertToBinary(text);
+            var keyBinary = ConvertToBinary(key.ToString());
+            Console.WriteLine("TextBinary");
+            Console.WriteLine(textBinary);
+            Console.WriteLine("KeyBinary");
+            Console.WriteLine(keyBinary);
+            var xor = Xor(text, key);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("ShifrText");
+            Console.WriteLine(xor);
+            Console.ResetColor();
+            Console.WriteLine(ConvertToBinary(xor));
+            Console.WriteLine("Decrypt");
+            Console.WriteLine(Xor(xor,key));
+        }
+        static string ConvertToBinary(string text)
+        {
+            string res="";
+            for (var i = 0; i < text.Length; i++)
+            {
+                res += Convert.ToString(text[i], 2).PadLeft(16,'0');
+            }
+            return res;
+        }
+
+        static string Xor(string text,StringBuilder key)
+        {
+            string res = "";
+            for (var i = 0; i < text.Length; i++)
+            {
+                res += (char)(text[i] ^ key[i]);
+            }
+
+            return res;
+        }
+    }
+}
+
